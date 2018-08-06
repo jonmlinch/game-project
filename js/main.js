1903373a5991416e9266107eb0e4d1e0c73ac916
Jonathan Linch <junglecat@Jonathans-MacBook-Pro.local>
2018-08-06 16:51:37 -0700

    cropped sprites from larger sprite sheet, removed background from cropped sprites

--- js/main.js
+++ js/main.js
@@ -3,13 +3,13 @@ console.log('main.js working')
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameboard', {preload: preload, create: create, update: update});
 
 function preload(){
-		
+	console.log('preload');
 };
 
 function create(){
-
+	console.log('create');
 };
 
 function update(){
-
+	console.log('update');
 };
