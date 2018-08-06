c8b6aaf939bfa703218f6f42ab4b4268e22057da
Jonathan Linch <junglecat@Jonathans-MacBook-Pro.local>
2018-08-06 13:28:25 -0700

    collected sprite sheets and audio to be used in the game, added title and game board

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
 console.log('main.js working')
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameboard', {preload: preload, create: create, update: update});
+
+function preload(){
+		
+};
+
+function create(){
+
+};
+
+function update(){
+
+};
