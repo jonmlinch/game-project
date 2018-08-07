5bbe4299fac0449e86daac17a82034ad953dd2f4
Jonathan Linch <junglecat@Jonathans-MacBook-Pro.local>
2018-08-07 11:25:37 -0700

    Photoshopped images to remove background and add surfboard to gorilla, added sunst background and still water background to gameboard, thrill now sits in upper left of board

--- js/main.js
+++ js/main.js
@@ -2,14 +2,32 @@ console.log('main.js working')
 
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameboard', {preload: preload, create: create, update: update});
 
+
 function preload(){
 	console.log('preload');
+	game.load.image('sunset', './assets/img/sunset-background.jpg');
+	game.load.image('horizon', './assets/img/flat-water-section.png');
+	game.load.image('pad1', './assets/img/thrilla-paddle-1.png');
 };
 
 function create(){
 	console.log('create');
+	//game images
+	var  background = game.add.tileSprite(0, 0, game.width, game.height, 'sunset');
+	background.scale.setTo(4)
+
+	game.add.tileSprite(0, 200, game.width, game.height - 200, 'horizon');
+
+	
+	var thrilla = game.add.sprite(0, 0, 'pad1');
+	thrilla.scale.setTo(1.5);
+	
+
+	//game animations
+
+	//game sounds
 };
 
 function update(){
 	console.log('update');
-};
+}
