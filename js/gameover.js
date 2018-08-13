0ccc0a174a2ca6b4cb0eaadd96934d2ed997b539
Jonathan Linch <junglecat@Jonathans-MacBook-Pro.local>
2018-08-13 11:45:30 -0700

    small adjustments to styling

--- js/gameover.js
+++ js/gameover.js
@@ -7,6 +7,7 @@ var loser = {
 		score = game.add.text(20, 20, 'You Are ' + thrilla.score.toString() + '% Drunk', {font: 'bold 35px Shadows Into Light'});
 		gameEnd = game.add.text(200, 100, 'Game Over!', {font: '60px Permanent Marker'});
 		butstill = game.add.text(120, 165, "Gorillas only surf when they're drunk", {font: '25px Permanent Marker'});
+		
 		//add camera movement to thrilla
 		game.camera.follow(thrilla);
 	},
--- js/startMenu.js
+++ js/startMenu.js
@@ -5,8 +5,12 @@ var startUp = {
 		theButton();
 		beach = game.add.audio('ocean', 0.9);
 		beach.play();
+		
 		//add score to game
 		score = game.add.text(20, 20, 'You Are ' + thrilla.score.toString() + '% Drunk', {font: 'bold 35px Shadows Into Light'})
+
+		//add camera movement to thrilla
+		game.camera.follow(thrilla);
 
 	}
 
